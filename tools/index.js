ff3708eda368123cde11cb20bff658561b1bc28a
Aswathy Suresh <aswathysuresh122006@gmail.com>
2026-02-11 21:43:45 +0530

    Create index.js

---
+++ tools/index.js
@@ -0,0 +1,24 @@
+const chalk = require("chalk");
+const inquirer = require("inquirer");
+const _ = require("lodash");
+const fs = require("fs");
+
+class instagram {
+    constructor(username, password) {
+        this.username = username;
+        this.password = password;
+    }
+    // leave empty for now; the main code uses cookie login
+}
+
+const print = (msg, type, line) => {
+    if (!type) console.log(msg);
+    if (type === "ok") console.log(chalk.green(`${line ? "\n" : ""}⊙ ${msg}`));
+    if (type === "wait") console.log(chalk.cyan.bold(`${line ? "\n" : ""}∞ ${msg}`));
+    if (type === "warn") console.log(chalk.yellow(`${line ? "\n" : ""}≉ ${msg}`));
+    if (type === "err") console.log(chalk.red(`${line ? "\n" : ""}⋈ ${msg}`));
+};
+
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+module.exports = { chalk, inquirer, _, fs, instagram, print, delay };
